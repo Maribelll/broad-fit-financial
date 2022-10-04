409bfb308995024bd526789f4d1abb3160f3c86d
Maribelll <48179911+Maribelll@users.noreply.github.com>
2022-10-04 11:59:48 +0300

    fix: blocktopabout

--- Components/about/Blocktop.js
+++ Components/about/Blocktop.js
@@ -1,11 +1,19 @@
 import styles from "../../styles/blocktopabout.module.scss";
 import Image from "next/image";
-import lines2 from "../../public/footer2.png";
+import lines1 from "../../public/about_line.png";
+import lines2 from "../../public/about_line2.png";
+import lines3 from "../../public/footer2.png";
 
 export function Blocktopabout() {
   return (
     <div className={styles.container_blocktop}>
+      <div className={styles.line2}>
+        <Image src={lines2} alt="" />
+      </div>
       <div className={styles.blocktop}>
+        <div className={styles.line1}>
+          <Image src={lines1} alt="" />
+        </div>
         <div className={styles.blocktop_text}>
           <p className={styles.gold}>ABOUT BROAD FIT FINANCIAL&mdash;</p>
           <h2>Your success and growth is our top priority.</h2>
@@ -26,7 +34,7 @@ export function Blocktopabout() {
         </div>
       </div>
       <div className={styles.blocktop_img2}>
-        <Image src={lines2} alt="" />
+        <Image src={lines3} alt="" />
       </div>
     </div>
   );
