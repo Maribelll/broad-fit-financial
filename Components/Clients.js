0288a4b3cb1195275d5261544050a5417edc5309
Maribelll <48179911+Maribelll@users.noreply.github.com>
2022-10-17 14:26:38 +0300

    fix: changes2

--- Components/Clients.js
+++ Components/Clients.js
@@ -18,19 +18,19 @@ export function Clients() {
       <h3>Memberships with</h3>
       <div className={styles.team}>
         <div className={styles.card_photo}>
-          <Image src={img1} height={62} width={auto} alt="" />
+          <Image src={img1} height={62} width={123} alt="" />
         </div>
         <div className={styles.card_photo}>
-          <Image src={img2} alt="" />
+          <Image src={img2} height={62} width={76} alt="" />
         </div>
         <div className={styles.card_photo}>
-          <Image src={img3} alt="" />
+          <Image src={img3} height={62} width={132} alt="" />
         </div>
         <div className={styles.card_photo}>
-          <Image src={img4} alt="" />
+          <Image src={img4} height={62} width={191} alt="" />
         </div>
         <div className={styles.card_photo}>
-          <Image src={img5} alt="" />
+          <Image src={img5} height={62} width={193.1} alt="" />
         </div>
       </div>
     </div>
