69f2975671f1e0d73ebd38c6129ca127f67adcd3
Maribelll <48179911+Maribelll@users.noreply.github.com>
2022-10-17 21:26:14 +0300

    fix:contact

--- Components/contact/Blocktop.js
+++ Components/contact/Blocktop.js
@@ -1,5 +1,7 @@
 import styles from "../../styles/blocktopcontact.module.scss";
-import { Action } from "../contact/Action";
+import lines1 from "../../public/Lines1_contact.svg";
+import lines2 from "../../public/lines2_contact.svg";
+import Image from "next/image";
 
 export function Blocktopcontact() {
   return (
@@ -7,10 +9,20 @@ export function Blocktopcontact() {
       <div className={styles.blocktop}>
         <div className={styles.blocktop_text}>
           <p className={styles.gold}>CONTACT&mdash;</p>
-          <h1 id="h2">We’re here to help.</h1>
+          <h1 style={{ color: "white" }} id="h2">
+            We’re here to help.
+          </h1>
+          <p>
+            To get in touch, call us at (561) 450-8454, send an email to
+            Stephanie@BroadFit.com, or fill out the form below.
+          </p>
         </div>
-        <div className={styles.blocktop_text2}></div>
-        <Action />
+        <div className={styles.blocktop_img}>
+          <Image src={lines1} alt="" />
+        </div>
+        <div className={styles.blocktop_img2}>
+          <Image src={lines2} alt="" />
+        </div>
       </div>
     </div>
   );
