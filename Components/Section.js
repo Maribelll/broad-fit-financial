42114eb57f0b18c24c81cc4f73400245a7cfc71d
Maribelll <48179911+Maribelll@users.noreply.github.com>
2022-11-28 21:38:15 +0300

    fix: changes markets

--- Components/Section.js
+++ Components/Section.js
@@ -1,20 +1,20 @@
 import Image from "next/image";
 import img1 from "../public/sectors/1-fitness.jpg";
-import img2 from "../public/sectors/2-restorative.jpg";
-import img3 from "../public/sectors/3-ymcas.jpg";
-import img4 from "../public/sectors/4-commercial.jpg";
-import img5 from "../public/sectors/5-corporate.jpg";
-import img6 from "../public/sectors/6-homeowners.jpg";
-import img7 from "../public/sectors/7-schools.jpg";
-import img8 from "../public/sectors/8-nutrition.jpg";
-import img9 from "../public/sectors/9-active.jpg";
-import img10 from "../public/sectors/10-health.jpg";
-import img11 from "../public/sectors/11-municipalities.jpg";
-import img12 from "../public/sectors/12-health.jpg";
-import img13 from "../public/sectors/13-municipalities.jpg";
-import img14 from "../public/sectors/14-hospitality.jpg";
-import img15 from "../public/sectors/15-recovery.jpg";
-import img16 from "../public/sectors/16-franchises.jpg";
+import img2 from "../public/sectors/2-sports.jpg";
+import img3 from "../public/sectors/3-performance.jpg";
+import img4 from "../public/sectors/4-multifamily.jpg";
+import img5 from "../public/sectors/5-rehab.jpg";
+import img6 from "../public/sectors/6-commercial.jpg";
+import img7 from "../public/sectors/9-active.jpg";
+import img8 from "../public/sectors/8-ymcas.jpg";
+import img9 from "../public/sectors/9-franchises.jpg";
+import img10 from "../public/sectors/9-active.jpg";
+import img11 from "../public/sectors/10-schools.jpg";
+import img12 from "../public/sectors/11-municipalities.jpg";
+import img13 from "../public/sectors/12-countryclubs.jpg";
+import img14 from "../public/sectors/13-corporate.jpg";
+import img15 from "../public/sectors/14-hospitality.jpg";
+import img16 from "../public/sectors/15-recovery.jpg";
 import styles from "../styles/section.module.scss";
 
 export function Section() {
@@ -26,8 +26,8 @@ export function Section() {
             <Image src={img1} alt="Fitness Centers" />
             <h4>Fitness Centers</h4>
             <p>
-              Helping companies in the fitness market acquire the equipment they
-              need to start, expand and grow their operations.
+              Helping companies acquire the equipment they need to open, expand,
+              and grow their operations.
             </p>
           </div>
           <div
@@ -35,11 +35,11 @@ export function Section() {
             data-aos-delay="100"
             className={styles.section_card}
           >
-            <Image src={img12} alt="Professional Sports" />
+            <Image src={img2} alt="Professional Sports" />
             <h4>Professional Sports</h4>
             <p>
-              Supporting professional sport teams with financing for sports
-              fields, basketball courts, locker rooms and more.
+              Supporting professional organizations with financing for fields,
+              courts, locker rooms, training facilities, score boards, and more.
             </p>
           </div>
           <div
@@ -47,16 +47,16 @@ export function Section() {
             data-aos-delay="200"
             className={styles.section_card}
           >
-            <Image src={img8} alt="Performance Training" />
+            <Image src={img3} alt="Performance Training" />
             <h4>Performance Training</h4>
-            <p>We finance equipment for performance training facilities.</p>
+            <p>Financing equipment for athletic training facilities.</p>
           </div>
           <div data-aos="fade-up" className={styles.section_card}>
-            <Image src={img6} alt="Homeowners’ Associations" />
-            <h4>Homeowners’ Associations</h4>
+            <Image src={img4} alt="Multi-Family" />
+            <h4>Multi-Family</h4>
             <p>
-              We finance equipment for HOAs/Condos and Co-ops with attractive
-              financing structures that meet their needs.
+              Financing equipment for HOAs, condos, apartments, and residential
+              communities.
             </p>
           </div>
           <div
@@ -64,12 +64,11 @@ export function Section() {
             data-aos-delay="100"
             className={styles.section_card}
           >
-            <Image src={img2} alt="Restorative Health" />
-            <h4>Restorative Health</h4>
+            <Image src={img5} alt=" Rehabilitation" />
+            <h4> Rehabilitation</h4>
             <p>
-              Supporting physical therapy businesses, spas and pain management
-              clinics acquire the equipment needed to help their clients
-              recover.
+              Helping physical therapy facilities, spas, and pain management
+              clinics finance recovery equipment.
             </p>
           </div>
           <div
@@ -77,50 +76,46 @@ export function Section() {
             data-aos-delay="200"
             className={styles.section_card}
           >
-            <Image src={img4} alt="Commercial Office Buildings" />
+            <Image src={img6} alt="Commercial Office Buildings" />
             <h4>Commercial Office Buildings</h4>
             <p>
-              We finance a wide array of collateral including fitness,
-              restaurant, and office equipment, as well as furniture for office
-              buildings.
+              Financing a wide array of collateral for office buildings
+              including fitness facilities, restaurants, corporate equipment,
+              and furniture.
             </p>
           </div>
           <div data-aos="fade-up" className={styles.section_card}>
-            <Image src={img9} alt="Active Aging" />
-            <h4>Active Aging</h4>
-            <p>Providing financing solutions for Active Aging Communities.</p>
+            <Image src={img7} alt="Senior Communities" />
+            <h4>Senior Communities</h4>
+            <p>Providing financing solutions for active senior communities.</p>
           </div>
           <div
             data-aos="fade-up"
             data-aos-delay="100"
             className={styles.section_card}
           >
-            <Image src={img3} alt="YMCAs, JCCs and Non-Profits" />
-            <h4>YMCAs, JCC, KROC Centers, &amp; Other Non-Profits</h4>
+            <Image src={img8} alt="YMCAs, JCCs and Non-Profits" />
+            <h4>YMCAs, JCCs, KROC Centers, &amp; Other Non-Profits:</h4>
             <p>
-              Non-profits play a sig­nif­i­cant role in their communities, and
-              we are happy to provide them with appealing financing options.
+              Supporting community organizations with playgrounds, fitness
+              centers, furniture, pools and more.
             </p>
           </div>
-          {/* <div
-            data-aos="fade-up"
-            data-aos-delay="200"
-            className={styles.section_card}
-          >
-            <Image src={img10} alt="Health Care Facilities" />
-            <h4>Health Care Facilities</h4>
-            <p>
-              Helping hospitals, clinics, dentists, physical therapists, private
-              practices and other healthcare professionals acquire equipment.
-            </p>
-          </div> */}
           <div data-aos="fade-up" className={styles.section_card}>
-            <Image src={img7} alt="Schools and Universities" />
-            <h4>Schools &amp; Universities</h4>
+            <Image src={img9} alt="Franchises" />
+            <h4>Franchises</h4>
             <p>
-              Whether refreshing existing equipment or outfitting a new
-              facility, financing can provide schools the ability to acquire new
-              assets when needed.
+              Equipment financing for franchises in the health and wellness
+              markets.
+            </p>
+          </div>
+
+          <div data-aos="fade-up" className={styles.section_card}>
+            <Image src={img11} alt="Еducation" />
+            <h4>Еducation</h4>
+            <p>
+              Partnering with schools and universities to provide financing for
+              all facility needs.
             </p>
           </div>
           <div
@@ -128,8 +123,8 @@ export function Section() {
             data-aos-delay="100"
             className={styles.section_card}
           >
-            <Image src={img11} alt="Municipalities" />
-            <h4>Municipalities</h4>
+            <Image src={img12} alt="Parks and Recreation" />
+            <h4>Parks &amp; Recreation</h4>
             <p>Specialized financing for government entities.</p>
           </div>
           <div
@@ -141,15 +136,14 @@ export function Section() {
               src={img13}
               alt="Country Clubs, Golf Clubs and Tennis Clubs"
             />
-            <h4>Country Clubs, Golf, Tennis, &amp; Swim Clubs</h4>
+            <h4>Country Clubs, Golf Clubs &amp; Tennis Clubs</h4>
             <p>
-              Helping equip sports clubs facilities with locker rooms, score
-              boards, lighting, bleachers, sports fields, basketball courts and
-              more.
+              Financing private and public facilities with locker rooms, fitness
+              centers, restaurants, lighting, greens, courts &amp; more.
             </p>
           </div>
           <div data-aos="fade-up" className={styles.section_card}>
-            <Image src={img5} alt="Corporate Wellness" />
+            <Image src={img14} alt="Corporate Wellness" />
             <h4>Corporate Wellness</h4>
             <p>
               Helping corporate workplaces promote health and wellness with the
@@ -161,11 +155,11 @@ export function Section() {
             data-aos-delay="100"
             className={styles.section_card}
           >
-            <Image src={img14} alt="Hospitality" />
+            <Image src={img15} alt="Hospitality" />
             <h4>Hospitality</h4>
             <p>
-              We work with hotels, resorts, and other hospitality-based
-              businesses to outfit their facilities.
+              Financing for hotels, resorts, and other hospitality-based
+              businesses to outfit their locations.
             </p>
           </div>
           <div
@@ -173,19 +167,11 @@ export function Section() {
             data-aos-delay="200"
             className={styles.section_card}
           >
-            <Image src={img15} alt="Recovery" />
+            <Image src={img16} alt="Recovery" />
             <h4>Recovery</h4>
             <p>
-              Offering equipment financing solutions for businesses providing
-              recovery and rehabilitation services.
-            </p>
-          </div>
-          <div data-aos="fade-up" className={styles.section_card}>
-            <Image src={img16} alt="Franchises" />
-            <h4>Franchises</h4>
-            <p>
-              Equipment financing for franchises in the health and wellness
-              markets.
+              Financing solutions for businesses providing recovery and
+              rehabilitation services.
             </p>
           </div>
         </div>
